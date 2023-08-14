916dbfdb5633a63bb15176644511100ba6b69a16
AvijitSen <senavijit1697@gmail.com>
2023-08-15 02:26:14 +0530

    add imageUrl field to user schema

--- Models/User.js
+++ Models/User.js
@@ -16,6 +16,9 @@ const userSchema = new Schema(
             type: String,
             required: true,
         },
+        imageUrl: {
+            type: String,
+        },
         role: {
             type: String,
             enum: ["admin", "creator", "student", "parent"],
