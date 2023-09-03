5840bd33dac97af38df1619021a688e3e3ca70f9
AvijitSen <senavijit1697@gmail.com>
2023-09-03 13:42:23 +0530

    modify validation error response, showing only one error in response

--- Validations/Validator.js
+++ Validations/Validator.js
@@ -4,17 +4,23 @@ module.exports = (req, res, next) => {
     const result = validationResult(req);
 
     if (!result.isEmpty()) {
-        const errors = result.errors.reduce((allErrors, error) => {
-            if (allErrors[error.path]) {
-                allErrors[error.path].push(error.msg);
-            } else {
-                allErrors[error.path] = [error.msg];
-            }
-            return allErrors;
-        }, {});
+        // const errors = result.errors.reduce((allErrors, error) => {
+        //     if (allErrors[error.path]) {
+        //         allErrors[error.path].push(error.msg);
+        //     } else {
+        //         allErrors[error.path] = [error.msg];
+        //     }
+        //     return allErrors;
+        // }, {});
+
+        const errors = result.errors;
+        const firstError = errors[0];
+
         return res.status(203).json({
             status: false,
-            errors: errors,
+            errors: {
+                [firstError.path]: firstError.msg,
+            },
         });
     }
     next();
