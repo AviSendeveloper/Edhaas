52bc674b8b17c6b667346f7ce576410f28031566
AvijitSen <senavijit1697@gmail.com>
2023-10-18 00:44:13 +0530

    change http method for exam get question

--- Routes/Parent/exam.route.js
+++ Routes/Parent/exam.route.js
@@ -2,6 +2,6 @@ const Router = require("express").Router();
 const ExamController = require("../../Controllers/Exam/ExamController");
 
 Router.post("/initiate", ExamController.initiateExam);
-Router.get("/get-question", ExamController.examList);
+Router.post("/get-question", ExamController.examList);
 
 module.exports = Router;
