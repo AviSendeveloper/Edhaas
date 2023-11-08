dd6c63a57beb89a20b828a76bb2c3c0e96d0e42c
AvijitSen <senavijit1697@gmail.com>
2023-11-09 01:08:44 +0530

    add log in initiateExam function again 3

--- Services/exam.service.js
+++ Services/exam.service.js
@@ -19,10 +19,10 @@ exports.initiateExam = async ({
         const endTime = new Date(
             new Date(startTime).getTime() + duration * 60 * 1000
         );
-        logger.info("startTime: ");
-        logger.info(startTime);
-        logger.info("duration: ");
-        logger.info(duration);
+        // logger.info("startTime: ");
+        // logger.info(startTime);
+        // logger.info("duration: ");
+        // logger.info(duration);
         logger.info("endTime: ");
         logger.info(endTime);
         const intiatedExamDetails = await Exam.create({
